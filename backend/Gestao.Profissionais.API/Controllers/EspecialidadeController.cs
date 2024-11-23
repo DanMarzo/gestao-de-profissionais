3ac1d8c5306bc37844dd934faab4fab9dcd1b841
DanMarzo <marzogildan@gmail.com>
2024-11-23 18:36:46 -0300

    Obter todas as especialidades

---
+++ backend/Gestao.Profissionais.API/Controllers/EspecialidadeController.cs
@@ -0,0 +1,19 @@
+﻿using Gestao.Profissionais.API.Application.Features.EspecialidadeFeatures.ObterEspecialidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gestao.Profissionais.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EspecialidadeController : ControllerBase
+{
+    private readonly IMediator mediator;
+    public EspecialidadeController(IMediator mediator) { this.mediator = mediator; }
+
+    [HttpGet]
+    public async Task<IActionResult> ObterTodas()
+    {
+        var entidades = await this.mediator.Send(new ObterEspecialidadesRequest());
+        return Ok(entidades);
+    }
+}
