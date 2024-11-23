da834481a25a6ef2c77fdde31c2b873da7f4db0c
DanMarzo <marzogildan@gmail.com>
2024-11-23 17:45:50 -0300

    Registrando Auto Mapper e Media TR

--- backend/Gestao.Profissionais.API/Application/ApplicationServices.cs
+++ backend/Gestao.Profissionais.API/Application/ApplicationServices.cs
@@ -7,6 +7,9 @@ public static class ApplicationServices
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        services.AddAutoMapper(typeof(ApplicationServices).Assembly);
+        services.AddMediatR(x => x.RegisterServicesFromAssemblies(typeof(ApplicationServices).Assembly));
+
         services.AddScoped<IRepository, Repository>();
         return services;
     }
