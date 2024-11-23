61706e9747e1a9cf9b16ca7e5683a1f2134926a7
DanMarzo <marzogildan@gmail.com>
2024-11-23 19:02:50 -0300

    Criando Mapping para produtos

---
+++ backend/Gestao.Profissionais.API/Application/Mappings/ProfissionalMapping.cs
@@ -0,0 +1,11 @@
+﻿using Gestao.Profissionais.API.Application.DTOs;
+
+namespace Gestao.Profissionais.API.Application.Mappings;
+
+public class ProfissionalMapping : Profile
+{
+    public ProfissionalMapping()
+    {
+        CreateMap<ProfissionalDTO, ProfissionalEntity>().ReverseMap();
+    }
+}
