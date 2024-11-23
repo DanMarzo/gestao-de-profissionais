1d8669d075a15ea5df835377f6da848b070f09b3
DanMarzo <marzogildan@gmail.com>
2024-11-23 17:54:48 -0300

    Metodo para obter especialidades

---
+++ backend/Gestao.Profissionais.API/Application/Features/EspecialidadeFeatures/ObterEspecialidades/ObterEspecialidadesHandler.cs
@@ -0,0 +1,28 @@
+﻿using Gestao.Profissionais.API.Application.Contracts;
+using Gestao.Profissionais.API.Application.DTOs;
+
+namespace Gestao.Profissionais.API.Application.Features.EspecialidadeFeatures.ObterEspecialidades;
+
+public class ObterEspecialidadesHandler : IRequestHandler<ObterEspecialidadesRequest, IEnumerable<EspecialidadeDTO>>
+{
+    private readonly IRepository repository;
+    private readonly ILogger<ObterEspecialidadesHandler> logger;
+    private readonly IMapper mapper;
+    public ObterEspecialidadesHandler(IRepository repository, ILogger<ObterEspecialidadesHandler> logger, IMapper mapper)
+    {
+        this.repository = repository;
+        this.logger = logger;
+        this.mapper = mapper;
+    }
+
+    public async Task<IEnumerable<EspecialidadeDTO>> Handle(ObterEspecialidadesRequest request, CancellationToken cancellationToken)
+    {
+        this.logger.LogInformation("Request: Lista de especialidades.");
+
+        var especialidades = await this.repository.GetEntities<EspecialidadeDTO>();
+        var especialidadesDto = this.mapper.Map<IEnumerable<EspecialidadeDTO>>(especialidades);
+
+        this.logger.LogInformation($"Response: Lista de especialidades {JsonSerializer.Serialize(especialidadesDto)}");
+        return especialidadesDto;
+    }
+}
---
+++ backend/Gestao.Profissionais.API/Application/Features/EspecialidadeFeatures/ObterEspecialidades/ObterEspecialidadesRequest.cs
@@ -0,0 +1,9 @@
+﻿using Gestao.Profissionais.API.Application.DTOs;
+using MediatR;
+
+namespace Gestao.Profissionais.API.Application.Features.EspecialidadeFeatures.ObterEspecialidades;
+
+public class ObterEspecialidadesRequest : IRequest<IEnumerable<EspecialidadeDTO>>
+{
+
+}
