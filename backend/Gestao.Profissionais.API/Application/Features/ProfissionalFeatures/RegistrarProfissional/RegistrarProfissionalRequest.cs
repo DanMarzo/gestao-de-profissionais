4b5da19dedbd3294adff2ac5a2b2cd21e26b7efa
DanMarzo <marzogildan@gmail.com>
2024-11-23 19:13:28 -0300

    implementando heranca para registro de profissional

--- backend/Gestao.Profissionais.API/Application/Features/ProfissionalFeatures/RegistrarProfissional/RegistrarProfissionalRequest.cs
+++ backend/Gestao.Profissionais.API/Application/Features/ProfissionalFeatures/RegistrarProfissional/RegistrarProfissionalRequest.cs
@@ -1,15 +1,6 @@
 ﻿using Gestao.Profissionais.API.Application.DTOs;
+using Gestao.Profissionais.API.Application.DTOs.ProfissionalDTOs;
 
 namespace Gestao.Profissionais.API.Application.Features.ProfissionalFeatures.RegistrarProfissional;
 
-public class RegistrarProfissionalRequest : IRequest<ResponseCreateAPIDTO<long>>
-{
-    public string Nome { get; set; }
-    public string NumeroDocumento { get; set; }
-    public long EspecialidadeId { get; set; }
-    public ProfissionalEntity CriarProfissional()
-    {
-        var profissional = new ProfissionalEntity(this.Nome, this.NumeroDocumento, this.EspecialidadeId);
-        return profissional;
-    }
-}
+public class RegistrarProfissionalRequest : ProfissionalDTO, IRequest<ResponseCreateAPIDTO<long>> { }
