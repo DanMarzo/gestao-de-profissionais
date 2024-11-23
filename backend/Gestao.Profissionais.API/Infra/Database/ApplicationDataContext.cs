2ba6599a81c50fe8d60488f1ad3481797f4dfb10
DanMarzo <marzogildan@gmail.com>
2024-11-23 18:39:05 -0300

    Aplicando configuracoes do EF por assembly

--- backend/Gestao.Profissionais.API/Infra/Database/ApplicationDataContext.cs
+++ backend/Gestao.Profissionais.API/Infra/Database/ApplicationDataContext.cs
@@ -5,4 +5,9 @@ namespace Gestao.Profissionais.API.Infra.Database;
 public sealed class ApplicationDataContext : DbContext
 {
     public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options) : base(options) { }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDataContext).Assembly);
+    }
 }
