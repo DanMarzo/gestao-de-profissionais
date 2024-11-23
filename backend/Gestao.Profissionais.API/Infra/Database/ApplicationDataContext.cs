4e71bbb10bb6155c1cfa2fe77496ba9cc1fcc252
DanMarzo <marzogildan@gmail.com>
2024-11-23 17:18:29 -0300

    Configuracao do contexto do banco

---
+++ backend/Gestao.Profissionais.API/Infra/Database/ApplicationDataContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Gestao.Profissionais.API.Infra.Database;
+
+public sealed class ApplicationDataContext : DbContext
+{
+    public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options) : base(options) { }
+}
