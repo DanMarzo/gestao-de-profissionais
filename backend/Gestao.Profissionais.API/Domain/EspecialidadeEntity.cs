d338222a2a3ac69dffed714eeccfa95b249639db
DanMarzo <marzogildan@gmail.com>
2024-11-23 17:39:04 -0300

    Realizando relacionamento faltante

--- backend/Gestao.Profissionais.API/Domain/EspecialidadeEntity.cs
+++ backend/Gestao.Profissionais.API/Domain/EspecialidadeEntity.cs
@@ -7,4 +7,5 @@ public sealed class EspecialidadeEntity
     public long Id { get; set; }
     public string Nome { get; set; }
     public TipoDocEspecialidadeEnum TipoDocumento { get; set; }
+    public ProfissionalEntity Profissional { get; set; }
 }
