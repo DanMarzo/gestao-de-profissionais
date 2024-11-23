249cff1d95b4c751e5b1fb964dc6bfb9e944af20
DanMarzo <marzogildan@gmail.com>
2024-11-23 19:02:28 -0300

    Criando DTO para profissional

---
+++ backend/Gestao.Profissionais.API/Application/DTOs/ProfissionalDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Gestao.Profissionais.API.Application.DTOs;
+
+public class ProfissionalDTO
+{
+    public long Id { get; set; }
+    public string Nome { get; set; }
+    public string NumeroDocumento { get; set; }
+    public long EspecialidadeId { get; set; }
+    public DateTime CriadoEm { get; set; }
+}
