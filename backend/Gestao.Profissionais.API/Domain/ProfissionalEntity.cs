b40ac1ef713d7595e96b253dd176fa3ab8302f64
DanMarzo <marzogildan@gmail.com>
2024-11-23 18:37:13 -0300

    Definindo construtor e sobrecarga

--- backend/Gestao.Profissionais.API/Domain/ProfissionalEntity.cs
+++ backend/Gestao.Profissionais.API/Domain/ProfissionalEntity.cs
@@ -2,6 +2,15 @@
 
 public sealed class ProfissionalEntity
 {
+    public ProfissionalEntity() { }
+    public ProfissionalEntity(string nome, string numeroDocumento, long especialidadeId)
+    {
+        this.Nome = nome;
+        this.NumeroDocumento = numeroDocumento;
+        this.EspecialidadeId = especialidadeId;
+        this.CriadoEm = DateTime.Now;
+    }
+
     public long Id { get; set; }
     public string Nome { get; set; }
     public string NumeroDocumento { get; set; }
