4cf4dc385323a1698d9e4a2a0ed5bef8211c385f
DanMarzo <marzogildan@gmail.com>
2024-11-23 19:03:25 -0300

    Feature para obter profissional por ID

---
+++ backend/Gestao.Profissionais.API/Application/Features/ProfissionalFeatures/ProfissionalPorId/ProfissionalPorIdHandler.cs
@@ -0,0 +1,28 @@
+﻿using Gestao.Profissionais.API.Application.Contracts;
+using Gestao.Profissionais.API.Application.DTOs;
+using Gestao.Profissionais.API.Application.Exceptions;
+using System.Net;
+
+namespace Gestao.Profissionais.API.Application.Features.ProfissionalFeatures.ProfissionalPorId;
+
+public class ProfissionalPorIdHandler : IRequestHandler<ProfissionalPorIdRequest, ProfissionalDTO>
+{
+    private readonly ILogger<ProfissionalPorIdHandler> logger;
+    private readonly IRepository repository;
+    private readonly IMapper mapper;
+
+    public ProfissionalPorIdHandler(ILogger<ProfissionalPorIdHandler> logger, IRepository repository, IMapper mapper)
+    {
+        this.logger = logger;
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public async Task<ProfissionalDTO> Handle(ProfissionalPorIdRequest request, CancellationToken cancellationToken)
+    {
+        var profissional = await this.repository.GetEntityAsync<ProfissionalEntity>(x => x.Id == request.Id)
+            ?? throw new ValidateException("Não foi possível localizar o profissional", HttpStatusCode.NotFound);
+        var profissionalDto = this.mapper.Map<ProfissionalDTO>(profissional);
+        return profissionalDto;
+    }
+}
---
+++ backend/Gestao.Profissionais.API/Application/Features/ProfissionalFeatures/ProfissionalPorId/ProfissionalPorIdRequest.cs
@@ -0,0 +1,10 @@
+﻿using Gestao.Profissionais.API.Application.DTOs;
+
+namespace Gestao.Profissionais.API.Application.Features.ProfissionalFeatures.ProfissionalPorId;
+
+public class ProfissionalPorIdRequest : IRequest<ProfissionalDTO>
+{
+    public ProfissionalPorIdRequest(long id) { this.Id = id; }
+
+    public long Id { get; private set; }
+}
