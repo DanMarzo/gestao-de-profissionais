ec09f849ca0aeab2a3dd90df029c94569e894216
DanMarzo <marzogildan@gmail.com>
2024-11-23 19:29:20 -0300

    Finalizando metodo para atualizar profissional

---
+++ backend/Gestao.Profissionais.API/Application/Features/ProfissionalFeatures/AtualizarProfissional/AtualizarProfissionalHandler.cs
@@ -0,0 +1,33 @@
+﻿using Gestao.Profissionais.API.Application.Contracts;
+using Gestao.Profissionais.API.Application.DTOs.ProfissionalDTOs;
+using Gestao.Profissionais.API.Application.Exceptions;
+using System.Net;
+
+namespace Gestao.Profissionais.API.Application.Features.ProfissionalFeatures.AtualizarProfissional;
+
+public class AtualizarProfissionalHandler : IRequestHandler<AtualizarProfissionalRequest, ProfissionalDetalhesDTO>
+{
+    private readonly IRepository repository;
+    private readonly ILogger<AtualizarProfissionalHandler> logger;
+    private readonly IMapper mapper;
+    public AtualizarProfissionalHandler(IRepository repository, ILogger<AtualizarProfissionalHandler> logger, IMapper mapper)
+    {
+        this.repository = repository;
+        this.logger = logger;
+        this.mapper = mapper;
+    }
+
+    public async Task<ProfissionalDetalhesDTO> Handle(AtualizarProfissionalRequest request, CancellationToken cancellationToken)
+    {
+        var profissional = await this.repository.GetEntityAsync<ProfissionalEntity>(x => x.Id == request.Id)
+            ?? throw new ValidateException("Profissional não localizada.", HttpStatusCode.NotFound);
+
+        profissional.EspecialidadeId = request.Profissional.EspecialidadeId;
+        profissional.NumeroDocumento = request.Profissional.NumeroDocumento;
+        profissional.Nome = request.Profissional.Nome;
+        await this.repository.UpdateAsync(profissional);
+
+        var profissionalDto = this.mapper.Map<ProfissionalDetalhesDTO>(profissional);
+        return profissionalDto;
+    }
+}
---
+++ backend/Gestao.Profissionais.API/Application/Features/ProfissionalFeatures/AtualizarProfissional/AtualizarProfissionalRequest.cs
@@ -0,0 +1,15 @@
+﻿using Gestao.Profissionais.API.Application.DTOs.ProfissionalDTOs;
+
+namespace Gestao.Profissionais.API.Application.Features.ProfissionalFeatures.AtualizarProfissional;
+
+public class AtualizarProfissionalRequest : IRequest<ProfissionalDetalhesDTO>
+{
+    public AtualizarProfissionalRequest(long id, ProfissionalDTO profissional)
+    {
+        this.Id = id;
+        this.Profissional = profissional;
+    }
+
+    public long Id { get; private set; }
+    public ProfissionalDTO Profissional { get; private set; }
+}
