290cffd977727f923fc51dde069a4f081fe91fc4
DanMarzo <marzogildan@gmail.com>
2024-11-23 18:40:59 -0300

    Feature para registrar profissionais

---
+++ backend/Gestao.Profissionais.API/Application/Features/ProfissionalFeatures/RegistrarProfissional/RegistrarProfissionalHandler.cs
@@ -0,0 +1,26 @@
+﻿using Gestao.Profissionais.API.Application.Contracts;
+using Gestao.Profissionais.API.Application.DTOs;
+using Gestao.Profissionais.API.Application.Exceptions;
+
+namespace Gestao.Profissionais.API.Application.Features.ProfissionalFeatures.RegistrarProfissional;
+
+public class RegistrarProfissionalHandler : IRequestHandler<RegistrarProfissionalRequest, ResponseCreateAPIDTO<long>>
+{
+    private readonly IRepository repository;
+    private readonly ILogger<RegistrarProfissionalHandler> logger;
+    public RegistrarProfissionalHandler(IRepository repository, ILogger<RegistrarProfissionalHandler> logger)
+    {
+        this.repository = repository;
+        this.logger = logger;
+    }
+
+    public async Task<ResponseCreateAPIDTO<long>> Handle(RegistrarProfissionalRequest request, CancellationToken cancellationToken)
+    {
+        var especialidadeExiste = await this.repository.EntityExists<EspecialidadeEntity>(x => x.Id == request.EspecialidadeId);
+        if (!especialidadeExiste)
+            throw new ValidateException($"Especialidade Id {request.EspecialidadeId} informada não localizada!");
+        var profissional = request.CriarProfissional();
+        await this.repository.AddAsync(profissional);
+        return new ResponseCreateAPIDTO<long>(profissional.Id);
+    }
+}
---
+++ backend/Gestao.Profissionais.API/Application/Features/ProfissionalFeatures/RegistrarProfissional/RegistrarProfissionalRequest.cs
@@ -0,0 +1,15 @@
+﻿using Gestao.Profissionais.API.Application.DTOs;
+
+namespace Gestao.Profissionais.API.Application.Features.ProfissionalFeatures.RegistrarProfissional;
+
+public class RegistrarProfissionalRequest : IRequest<ResponseCreateAPIDTO<long>>
+{
+    public string Nome { get; set; }
+    public string NumeroDocumento { get; set; }
+    public long EspecialidadeId { get; set; }
+    public ProfissionalEntity CriarProfissional()
+    {
+        var profissional = new ProfissionalEntity(this.Nome, this.NumeroDocumento, this.EspecialidadeId);
+        return profissional;
+    }
+}
