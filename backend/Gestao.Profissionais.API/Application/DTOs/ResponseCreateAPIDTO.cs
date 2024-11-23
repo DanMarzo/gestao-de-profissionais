420944f60b34781120b10b9a5566285300ad36d2
DanMarzo <marzogildan@gmail.com>
2024-11-23 18:35:17 -0300

    DTO para response de itens criados

---
+++ backend/Gestao.Profissionais.API/Application/DTOs/ResponseCreateAPIDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Gestao.Profissionais.API.Application.DTOs;
+
+public class ResponseCreateAPIDTO<T>
+{
+    public ResponseCreateAPIDTO(T id)
+    {
+        this.Id = id;
+    }
+    public T Id { get; set; }
+}
