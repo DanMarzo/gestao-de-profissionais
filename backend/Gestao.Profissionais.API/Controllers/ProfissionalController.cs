6a755ba68978012bf42eb0a78274bdca3fb52924
DanMarzo <marzogildan@gmail.com>
2024-11-23 18:39:49 -0300

    Endpoint para registrar profissionais

---
+++ backend/Gestao.Profissionais.API/Controllers/ProfissionalController.cs
@@ -0,0 +1,19 @@
+﻿using Gestao.Profissionais.API.Application.Features.ProfissionalFeatures.RegistrarProfissional;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gestao.Profissionais.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProfissionalController : ControllerBase
+{
+    private readonly IMediator mediator;
+    public ProfissionalController(IMediator mediator) { this.mediator = mediator; }
+
+    [HttpPost]
+    public async Task<IActionResult> Registrar([FromBody] RegistrarProfissionalRequest request)
+    {
+        var profissionalId = await mediator.Send(request);
+        return Created($"{profissionalId.Id}", profissionalId);
+    }
+}
