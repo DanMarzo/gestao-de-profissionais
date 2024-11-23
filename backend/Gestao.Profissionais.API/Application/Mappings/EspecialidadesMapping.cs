8ed000ae40aba380e98431e9088e05ab99d6de99
DanMarzo <marzogildan@gmail.com>
2024-11-23 17:42:41 -0300

    Definindo mapeamento para especialidade

---
+++ backend/Gestao.Profissionais.API/Application/Mappings/EspecialidadesMapping.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using Gestao.Profissionais.API.Application.DTOs;
+
+namespace Gestao.Profissionais.API.Application.Mappings;
+
+public class EspecialidadesMapping : Profile
+{
+    public EspecialidadesMapping()
+    {
+        CreateMap<EspecialidadeDTO, EspecialidadeEntity>().ReverseMap();
+    }
+}
