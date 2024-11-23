3c594c7b66b123ac3798db7526a31d25ff038eb0
DanMarzo <marzogildan@gmail.com>
2024-11-23 17:42:04 -0300

    Definindo DTO para especialidade

---
+++ backend/Gestao.Profissionais.API/Application/DTOs/EspecialidadeDTO.cs
@@ -0,0 +1,10 @@
+﻿using Gestao.Profissionais.API.Domain.Enums;
+
+namespace Gestao.Profissionais.API.Application.DTOs;
+
+public sealed class EspecialidadeDTO
+{
+    public long Id { get; set; }
+    public string Nome { get; set; }
+    public TipoDocEspecialidadeEnum TipoDocumento { get; set; }
+}
