43ad9fd3454913246ca6efefe2858ff09e19d76a
DanMarzo <marzogildan@gmail.com>
2024-11-23 17:18:13 -0300

    Inclusao de servico do banco de dados no container DI

---
+++ backend/Gestao.Profissionais.API/Infra/InfraServices.cs
@@ -0,0 +1,14 @@
+﻿using Gestao.Profissionais.API.Infra.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gestao.Profissionais.API.Infra;
+
+public static class InfraServices
+{
+    public static IServiceCollection AddInfraServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connection = configuration.GetConnectionString("Default") ?? throw new InvalidOperationException("ConnectionString não localizada.");
+        services.AddDbContext<ApplicationDataContext>(opt => opt.UseSqlite(connection));
+        return services;
+    }
+}
