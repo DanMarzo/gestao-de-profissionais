c96c86415b4d42c543d4bcf84b1b0dcd2d12e69a
DanMarzo <marzogildan@gmail.com>
2024-11-24 01:19:24 -0300

    Use effects para observar o item selecionado

--- clients/gestao.profissionais.webapp/src/presentation/home/registrar-profissional/RegistrarProssionalModal.view-model.tsx
+++ clients/gestao.profissionais.webapp/src/presentation/home/registrar-profissional/RegistrarProssionalModal.view-model.tsx
@@ -2,7 +2,11 @@ import { useState } from "react";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
-import { EspecialidadeModel } from "../../../models/especialidade.model";
+import {
+  EspecialidadeModel,
+  nomeTipoDocEspecialidadeEnum,
+} from "../../../models/especialidade.model";
+import { obterEspecialidadesService } from "../../../infra/services/obter-especialidades.service";
 
 const formValidationSchema = yup.object().shape({
   nome: yup
@@ -17,10 +21,49 @@ const formValidationSchema = yup.object().shape({
 });
 
 const useRegistrarProssionalViewModel = () => {
+  const [loading, setLoading] = useState(false);
   const [especialidades, setEspecialidades] = useState<
     Array<EspecialidadeModel>
   >([]);
-  const [loading, setLoading] = useState(false);
+
+  const [especialidadeSelect, setEspecialidadeSelect] = useState<number | null>(
+    null
+  );
+  const [tipoDocField, setTipoDocField] = useState<string | null>("");
+
+  const obterEspecialidades = async () =>
+    obterEspecialidadesService()
+      .then((result) => {
+        console.log(result);
+        if (result.error) {
+          console.log(result);
+          //notificacao
+        } else {
+          setEspecialidades(result.data ?? []);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        location.reload();
+        //notificacao
+      })
+      .finally(() => setLoading(false));
+
+  useEffect(() => {
+    if (especialidades.length == 0) {
+      obterEspecialidades();
+    }
+    return () => {
+      setEspecialidades([]);
+    };
+  }, []);
+
+  useEffect(() => {
+    const tipoDoc = especialidades.find((i) => i.id == especialidadeSelect);
+    setTipoDocField(nomeTipoDocEspecialidadeEnum(tipoDoc?.tipoDocumento)); //
+    return () => {};
+  }, [especialidadeSelect]);
+
   const {
     register: registerForm,
     handleSubmit,
