fd4f7ee43b8e3cf5c40edef6eeebac2f757c449b
DanMarzo <marzogildan@gmail.com>
2024-11-23 23:00:27 -0300

    Preparando tabela para receber lista de profissionais

---
+++ clients/gestao.profissionais.webapp/src/presentation/home/components/TableListProfissionais.component.tsx
@@ -0,0 +1,42 @@
+import { ProfissionalModel } from "../../../models/profissional.model";
+
+type Props = {
+  profissionais: Array<ProfissionalModel>;
+};
+
+const TableListProfissionaisComponent = ({ profissionais }: Props) => {
+  return (
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nome</th>
+          <th scope="col">Especialidade</th>
+          <th scope="col">Tipo do Documento</th>
+          <th scope="col">Número do Documento</th>
+          <th scope="col">Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        {profissionais.map((profissional, index) => {
+          return (
+            <tr key={index}>
+              <th>{profissional.id}</th>
+              <th>{profissional.nome}</th>
+              <th>Especialidade</th>
+              <th>Tipo documento</th>
+              <th>{profissional.numeroDocumento}</th>
+              <th>
+                <button className="btn">
+                  <i className="bi bi-three-dots-vertical"></i>
+                </button>
+              </th>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default TableListProfissionaisComponent;
