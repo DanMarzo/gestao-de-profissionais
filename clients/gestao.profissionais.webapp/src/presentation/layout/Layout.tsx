a3a79ae60f6ba75a450eaef84695eb9c68366780
DanMarzo <marzogildan@gmail.com>
2024-11-23 22:10:35 -0300

    Definindo layout da aplicacao

--- clients/gestao.profissionais.webapp/src/presentation/layout/Layout.tsx
+++ clients/gestao.profissionais.webapp/src/presentation/layout/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <>
-      <div className="container bg-primary">
+      <header className="container bg-primary">
         <nav className="navbar navbar-expand-lg bg-primary">
           <div className="container-fluid">
             <a className="navbar-brand text-white" href="#">
@@ -12,8 +12,12 @@ const Layout = () => {
             <i className="bi bi-people-fill text-white"></i>
           </div>
         </nav>
+      </header>
+      <div className="container">
+        <main role="main" className="pb-3">
+          <Outlet />
+        </main>
       </div>
-      <Outlet />
     </>
   );
 };
