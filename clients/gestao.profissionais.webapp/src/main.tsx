8846e78c5a26fc31c11ca106e4785d648eb48b85
DanMarzo <marzogildan@gmail.com>
2024-11-24 19:58:26 -0300

    Inclusao de React Toastify

--- clients/gestao.profissionais.webapp/src/main.tsx
+++ clients/gestao.profissionais.webapp/src/main.tsx
@@ -4,7 +4,12 @@ import { routes } from "./presentation/router.tsx";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./main.scss";
 import "bootstrap-icons/font/bootstrap-icons.scss";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={routes} />
+  <>
+    <RouterProvider router={routes} />
+    <ToastContainer />
+  </>
 );
