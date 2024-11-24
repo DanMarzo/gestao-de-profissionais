c061e72f6d117d6f47f96642b3f153697d073bb4
DanMarzo <marzogildan@gmail.com>
2024-11-23 22:05:39 -0300

    Definindo appbar

--- clients/gestao.profissionais.webapp/src/main.tsx
+++ clients/gestao.profissionais.webapp/src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider } from "react-router-dom";
 import { routes } from "./presentation/router.tsx";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./main.scss";
+import "bootstrap-icons/font/bootstrap-icons.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- clients/gestao.profissionais.webapp/src/presentation/layout/Layout.tsx
+++ clients/gestao.profissionais.webapp/src/presentation/layout/Layout.tsx
@@ -3,6 +3,16 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <>
+      <div className="container bg-primary">
+        <nav className="navbar navbar-expand-lg bg-primary">
+          <div className="container-fluid">
+            <a className="navbar-brand text-white" href="#">
+              Gestão de Profissionais
+            </a>
+            <i className="bi bi-people-fill text-white"></i>
+          </div>
+        </nav>
+      </div>
       <Outlet />
     </>
   );
