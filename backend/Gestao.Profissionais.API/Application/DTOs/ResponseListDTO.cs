034961e0b05a74a7a70313c98db29faef85a492b
DanMarzo <marzogildan@gmail.com>
2024-11-24 21:01:36 -0300

    Classe generica para response de listagen.

---
+++ backend/Gestao.Profissionais.API/Application/DTOs/ResponseListDTO.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Gestao.Profissionais.API.Application.DTOs;
+
+public class ResponseListDTO<T>
+{
+    public ResponseListDTO(RequestListDTO request, int totalItens)
+    {
+        this.Qtde = request.Qtde;
+        this.Indice = request.Indice;
+        this.NroPaginas = this.CalcularNroPaginas(totalItens);
+        this.TotalItens = totalItens;
+    }
+
+    private int CalcularNroPaginas(int total)
+    {
+        var nroPaginas = (int)Math.Ceiling(total / (double)this.Qtde);
+        return nroPaginas;
+    }
+
+    [JsonPropertyName("qtde")]
+    public int Qtde { get; }
+    [JsonPropertyName("nroPaginas")]
+    public int NroPaginas { get; }
+    [JsonPropertyName("totalItens")]
+    public int TotalItens { get; }
+    [JsonPropertyName("indice")]
+    public int Indice { get; }
+    [JsonPropertyName("data")]
+    public IEnumerable<T> Data { get; set; }
+}
