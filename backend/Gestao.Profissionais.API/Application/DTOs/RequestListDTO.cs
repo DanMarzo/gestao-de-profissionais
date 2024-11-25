c916949c6431fa017aeb4221a1d185d2fba57d55
DanMarzo <marzogildan@gmail.com>
2024-11-24 21:00:55 -0300

    Classe utilizada para fazer requisicao de listagem, utilizada para heranca em metodos que necessitam de listagem

---
+++ backend/Gestao.Profissionais.API/Application/DTOs/RequestListDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace Gestao.Profissionais.API.Application.DTOs;
+
+public class RequestListDTO
+{
+    public RequestListDTO(int indice)
+    {
+        this.Indice = indice;
+    }
+    public RequestListDTO(int indice, int? qtde)
+    {
+        this.Indice = indice;
+        this.Qtde = qtde ?? 10;
+    }
+
+    public int Indice { get; }
+    public int Qtde { get; } = 10;
+
+    public int CalcularItensAPular()
+    {
+        int pular = (this.Indice - 1) * this.Qtde;
+        return pular;
+    }
+}
