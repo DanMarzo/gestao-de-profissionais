1088401b0f36eac0806ec8eec10651e9ca256e55
DanMarzo <marzogildan@gmail.com>
2024-11-25 14:47:10 -0300

    Inclusao de novo filtro na busca

--- clients/gestao.profissionais.webapp/src/infra/services/obter-profissionais.service.ts
+++ clients/gestao.profissionais.webapp/src/infra/services/obter-profissionais.service.ts
@@ -4,11 +4,13 @@ import { ResponseListDTO } from "./response/response-list.dto";
 
 const obterProfissionaisService = async (
   pagina: number,
+  especialidadeId?: number
 ): Promise<ResponseListDTO<ProfissionalModel>> => {
   //api/Profissional?pagina=1&itens=1
   //Caso sobre tempo, incluir o numero de itens que é opcional
   const response = await http.get("api/Profissional", { params: {
-    pagina
+    pagina,
+    especialidadeId
   } });
   return new ResponseListDTO<ProfissionalModel>(response);
 };
