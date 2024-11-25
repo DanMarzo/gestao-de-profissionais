b33f8e6f2b691968cd4b8fa26edfb3fa9e95dbe7
DanMarzo <marzogildan@gmail.com>
2024-11-25 16:40:09 -0300

    Atualizando custom hook

--- clients/gestao.profissionais.webapp/src/presentation/home/HomeView.view-model.ts
+++ clients/gestao.profissionais.webapp/src/presentation/home/HomeView.view-model.ts
@@ -1,17 +1,20 @@
-import { useContext, useEffect, useState } from "react";
-import { ProfissionalModel } from "../../models/profissional.model";
-import { ResponseListDTO } from "../../infra/services/response/response-list.dto";
-import { useSearchParams } from "react-router-dom";
-import { obterProfissionaisService } from "../../infra/services/obter-profissionais.service";
-import { toast } from "react-toastify";
+import { useContext, useState } from "react";
 import { EspecialidadeContext } from "../../providers/Especialidade.context";
 import { EspecialidadeModel } from "../../models/especialidade.model";
+import { ProfissionalContext } from "../../providers/Profissional.context";
+import { useSearchParams } from "react-router-dom";
 
 const HomeViewViewModel = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [listProfissionais, setListProfissionais] =
-    useState<ResponseListDTO<ProfissionalModel> | null>();
-  const [loading, setLoading] = useState(false);
+  const {
+    nextPage,
+    previousPage,
+    profissionais,
+    obterIndicePagina,
+    carregando,
+    handleIndiceEspecialidade,
+  } = useContext(ProfissionalContext);
+
+  const [_, setSearchParams]  = useSearchParams()
   const { carregando: carregandoEsp, especialidades } =
     useContext(EspecialidadeContext);
 
@@ -20,75 +23,19 @@ const HomeViewViewModel = () => {
 
   const handleSetEspecialidade = (especialidade: EspecialidadeModel | null) => {
     const indice = obterIndicePagina();
-    setSearchParams({
-      indice: indice.toString(),
-      especialidade: especialidade ? especialidade!.id.toString() : "",
-    });
+    handleIndiceEspecialidade(indice, especialidade?.id);
     setEspecialidadeSelecionada(especialidade);
-  };
-  const obterProfissionais = () => {
-    const indice = obterIndicePagina();
-    const especialidadeId = obterEspecialidadeId();
-    setLoading(true);
-    obterProfissionaisService(indice, especialidadeId)
-      .then((res) => {
-        if (!res.error) {
-          setListProfissionais(res);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        toast("Não foi possível carregar profissionais.", { type: "error" });
-        setListProfissionais(null);
-      })
-      .finally(() => setLoading(false));
-  };
-
-  useEffect(() => {
-    obterProfissionais();
-    return () => {};
-  }, [searchParams]);
-
-  const obterIndicePagina = () => {
-    const indice = searchParams.get("indice");
-    if (!indice) return 1;
-    return Number.parseInt(indice);
-  };
-
-  const obterEspecialidadeId = () => {
-    const especialidade = searchParams.get("especialidade");
-    if (!especialidade) return undefined;
-    return Number.parseInt(especialidade);
-  };
-  const nextPage = () => {
-    if (
-      listProfissionais &&
-      !listProfissionais.error &&
-      obterIndicePagina() < listProfissionais.nroPaginas
-    ) {
-      setSearchParams({ indice: (obterIndicePagina() + 1).toString() });
-    }
-  };
-  const previousPage = () => {
-    if (
-      listProfissionais &&
-      !listProfissionais.error &&
-      obterIndicePagina() > 1
-    ) {
-      setSearchParams({ indice: (obterIndicePagina() - 1).toString() });
-    }
   };
 
   return {
     especialidades,
-    loading,
-    listProfissionais,
-    setSearchParams,
+    carregando,
+    profissionais,
     nextPage,
     previousPage,
     carregandoEsp,
     especialidadeSelecionada,
-    handleSetEspecialidade,
+    handleSetEspecialidade,setSearchParams
   };
 };
 
