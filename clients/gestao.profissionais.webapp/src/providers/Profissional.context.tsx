efa94dbc76cd4de7ac8a1ef6a434247df4a0ed93
DanMarzo <marzogildan@gmail.com>
2024-11-25 16:40:20 -0300

    Novo contexto para profissional

---
+++ clients/gestao.profissionais.webapp/src/providers/Profissional.context.tsx
@@ -0,0 +1,104 @@
+import { createContext, PropsWithChildren, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { ProfissionalModel } from "../models/profissional.model";
+import { obterProfissionaisService } from "../infra/services/obter-profissionais.service";
+import { useSearchParams } from "react-router-dom";
+import { ResponseListDTO } from "../infra/services/response/response-list.dto";
+
+interface ProfissionalContextProps {
+  profissionais: ResponseListDTO<ProfissionalModel> | null;
+  carregando: boolean;
+  obterProfissionais: () => void;
+  nextPage: () => void;
+  previousPage: () => void;
+  obterIndicePagina: () => number;
+  handleIndiceEspecialidade: (indice: number, especialidade?: number) => void;
+}
+
+const ProfissionalContext = createContext<ProfissionalContextProps>(
+  {} as ProfissionalContextProps
+);
+
+const ProfissionalProvider = ({ children }: PropsWithChildren) => {
+  const [profissionais, setProfissionais] =
+    useState<ResponseListDTO<ProfissionalModel> | null>(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [carregando, setCarregando] = useState(false);
+
+  const obterProfissionais = () => {
+    const indice = obterIndicePagina();
+    const especialidadeId = obterEspecialidadeId();
+    setCarregando(true);
+    obterProfissionaisService(indice, especialidadeId)
+      .then((res) => {
+        if (!res.error) {
+          setProfissionais(res);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        toast("Não foi possível carregar profissionais.", { type: "error" });
+        setProfissionais(null);
+      })
+      .finally(() => setCarregando(false));
+  };
+
+  useEffect(() => {
+    obterProfissionais();
+    return () => {};
+  }, [searchParams]);
+
+  const obterIndicePagina = () => {
+    const indice = searchParams.get("indice");
+    if (!indice) return 1;
+    return Number.parseInt(indice);
+  };
+
+  const obterEspecialidadeId = () => {
+    const especialidade = searchParams.get("especialidade");
+    if (!especialidade) return undefined;
+    return Number.parseInt(especialidade);
+  };
+  const nextPage = () => {
+    if (
+      profissionais &&
+      !profissionais.error &&
+      obterIndicePagina() < profissionais.nroPaginas
+    ) {
+      setSearchParams({ indice: (obterIndicePagina() + 1).toString() });
+    }
+  };
+  const previousPage = () => {
+    if (profissionais && !profissionais.error && obterIndicePagina() > 1) {
+      setSearchParams({ indice: (obterIndicePagina() - 1).toString() });
+    }
+  };
+  const handleIndiceEspecialidade = (
+    indice: number,
+    especialidade?: number
+  ) => {
+    setSearchParams({
+      indice: indice.toString(),
+      especialidade: especialidade ? especialidade.toString() : "",
+    });
+  };
+  return (
+    <ProfissionalContext.Provider
+      value={{
+        handleIndiceEspecialidade,
+        obterIndicePagina,
+        profissionais,
+        carregando,
+        obterProfissionais,
+        nextPage,
+        previousPage,
+      }}
+    >
+      {children}
+    </ProfissionalContext.Provider>
+  );
+};
+
+export type { ProfissionalContextProps };
+export { ProfissionalProvider, ProfissionalContext };
