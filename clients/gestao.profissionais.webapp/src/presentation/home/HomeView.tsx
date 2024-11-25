6018dcb7703c16ac7dab886d90debdaf8fe8282c
DanMarzo <marzogildan@gmail.com>
2024-11-25 16:39:51 -0300

    Envolvendo em provider

--- clients/gestao.profissionais.webapp/src/presentation/home/HomeView.tsx
+++ clients/gestao.profissionais.webapp/src/presentation/home/HomeView.tsx
@@ -11,124 +11,131 @@ const RegistrarProfissionalModal = lazy(
 
 const HomeView = () => {
   const {
-    listProfissionais,
-    setSearchParams,
+    profissionais,
     nextPage,
     previousPage,
     especialidades,
     especialidadeSelecionada,
     handleSetEspecialidade,
+    setSearchParams,
   } = HomeViewViewModel();
 
   return (
     <Suspense>
-      <main role="main" className="container-fluid p-4 gu">
-        <div className="row mb-4">
-          <div className="col-6 p-0 ">
-            <div className="dropdown">
-              <button
-                className="btn btn-secondary dropdown-toggle text-primary"
-                type="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Filtrar por Especialidade:{" "}
-                {`${especialidadeSelecionada?.nome ?? "Todas"}`}
-              </button>
-              <ul className="dropdown-menu">
-                <li>
-                  <h6 className="dropdown-header">FILTRAR POR:</h6>
-                </li>
+      <>
+        <main role="main" className="container-fluid p-4 gu">
+          <div className="row mb-4">
+            <div className="col-6 p-0 ">
+              <div className="dropdown">
+                <button
+                  className="btn btn-secondary dropdown-toggle text-primary"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Filtrar por Especialidade:{" "}
+                  {`${especialidadeSelecionada?.nome ?? "Todas"}`}
+                </button>
+                <ul className="dropdown-menu">
+                  <li>
+                    <h6 className="dropdown-header">FILTRAR POR:</h6>
+                  </li>
 
-                <li>
-                  <button
-                    onClick={() => handleSetEspecialidade(null)}
-                    className="dropdown-item"
-                  >
-                    Todas
-                  </button>
-                </li>
-                {especialidades.map((item, index) => {
-                  return (
-                    <li key={index}>
-                      <button
-                        onClick={() => handleSetEspecialidade(item)}
-                        className="dropdown-item"
-                      >
-                        {item.nome}
-                      </button>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
-          <div className="col-6 d-flex justify-content-end p-0">
-            <RegistrarProfissionalModal />
-          </div>
-        </div>
-        <div className="row mb-4">
-          <TableProfissionais profissionais={listProfissionais?.data ?? []} />
-        </div>
-        <div className="row">
-          <nav
-            className="col-12 p-0 d-flex justify-content-center"
-            aria-label="Navegar entre os itens"
-          >
-            {listProfissionais?.error ? (
-              <></>
-            ) : (
-              <nav aria-label="Page navigation example">
-                <ul className="pagination">
-                  <li className="page-item">
+                  <li>
                     <button
-                      onClick={() => previousPage()}
-                      className="page-link"
-                      aria-label="Previous"
+                      onClick={() => handleSetEspecialidade(null)}
+                      className="dropdown-item"
                     >
-                      <span aria-hidden="true">&laquo;</span>
+                      Todas
                     </button>
                   </li>
-                  {/* Listagem */}
-                  {Array.from({
-                    length: listProfissionais?.nroPaginas ?? 1,
-                  }).map((_, index) => {
+                  {especialidades.map((item, index) => {
                     return (
-                      <li
-                        key={index}
-                        className={`${
-                          listProfissionais?.indice == index + 1
-                            ? "page-item active"
-                            : "page-item"
-                        }`}
-                      >
+                      <li key={index}>
                         <button
-                          onClick={() =>
-                            setSearchParams({ indice: (index + 1).toString() })
-                          }
-                          className="page-link"
+                          onClick={() => handleSetEspecialidade(item)}
+                          className="dropdown-item"
                         >
-                          {index + 1}
+                          {item.nome}
                         </button>
                       </li>
                     );
                   })}
-
-                  <li className="page-item">
-                    <button
-                      onClick={() => nextPage()}
-                      className="page-link"
-                      aria-label="Next"
-                    >
-                      <span aria-hidden="true">&raquo;</span>
-                    </button>
-                  </li>
                 </ul>
-              </nav>
-            )}
-          </nav>
-        </div>
-      </main>
+              </div>
+            </div>
+            <div className="col-6 d-flex justify-content-end p-0">
+              <RegistrarProfissionalModal />
+            </div>
+          </div>
+          <div className="row mb-4">
+            <TableProfissionais profissionais={profissionais?.data ?? []} />
+          </div>
+          <div className="row">
+            <nav
+              className="col-12 p-0 d-flex justify-content-center"
+              aria-label="Navegar entre os itens"
+            >
+              {profissionais?.error ? (
+                <></>
+              ) : (
+                <nav aria-label="Page navigation example">
+                  <ul className="pagination">
+                    <li className="page-item">
+                      <button
+                        onClick={() => previousPage()}
+                        className="page-link"
+                        aria-label="Previous"
+                      >
+                        <span aria-hidden="true">&laquo;</span>
+                      </button>
+                    </li>
+                    {/* Listagem */}
+                    {Array.from({
+                      length: profissionais?.nroPaginas ?? 1,
+                    }).map((_, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className={`${
+                            profissionais?.indice == index + 1
+                              ? "page-item active"
+                              : "page-item"
+                          }`}
+                        >
+                          <button
+                            onClick={() =>
+                              setSearchParams((values) => {
+                                return {
+                                  ...values,
+                                  indice: (index + 1).toString(),
+                                };
+                              })
+                            }
+                            className="page-link"
+                          >
+                            {index + 1}
+                          </button>
+                        </li>
+                      );
+                    })}
+
+                    <li className="page-item">
+                      <button
+                        onClick={() => nextPage()}
+                        className="page-link"
+                        aria-label="Next"
+                      >
+                        <span aria-hidden="true">&raquo;</span>
+                      </button>
+                    </li>
+                  </ul>
+                </nav>
+              )}
+            </nav>
+          </div>
+        </main>
+      </>
     </Suspense>
   );
 };
