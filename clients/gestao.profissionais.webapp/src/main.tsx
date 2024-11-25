c3e4da44a99039c31fc562b0bd22d8cebc19539e
DanMarzo <marzogildan@gmail.com>
2024-11-25 14:46:56 -0300

    Configuracao para trabalhar com popperjs

--- clients/gestao.profissionais.webapp/src/main.tsx
+++ clients/gestao.profissionais.webapp/src/main.tsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./presentation/router.tsx";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import "./main.scss";
-import "bootstrap-icons/font/bootstrap-icons.scss";
-import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import { EspecialidadeProvider } from "./providers/Especialidade.context.tsx";
+import "./main.scss";
+import "bootstrap-icons/font/bootstrap-icons.scss";
+import "bootstrap";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")!).render(
   <EspecialidadeProvider>
