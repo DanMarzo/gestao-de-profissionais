c5f8ad63f6f6749e0c637dd7674373edc8aab214
DanMarzo <marzogildan@gmail.com>
2024-11-25 18:39:31 -0300

    Excluindo comentarios

--- clients/gestao.profissionais.webapp/src/infra/http.ts
+++ clients/gestao.profissionais.webapp/src/infra/http.ts
@@ -5,13 +5,11 @@ const baseURL = import.meta.env.VITE_URL_API;
 const http = axios.create({ baseURL });
 http.interceptors.response.use(
   function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
     return Promise.resolve(response);
   },
   function (error) {
     if (error instanceof AxiosError) {
-        //Em caso de erro do tipo axios, faca a tratativa para nao cair no catch
+      //Em caso de erro do tipo axios, faca a tratativa para nao cair no catch
       return Promise.resolve(error);
     }
     return Promise.reject(error);
