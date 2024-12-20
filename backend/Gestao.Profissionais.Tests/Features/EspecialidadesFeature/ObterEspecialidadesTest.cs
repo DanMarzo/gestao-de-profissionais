4ad30350f77359f81a6dbe539e7d80fa10a38873
dan.gil <dan.gil@DANGIL1>
2024-12-20 12:05:29 -0300

    Teste para obter especialidades

---
+++ backend/Gestao.Profissionais.Tests/Features/EspecialidadesFeature/ObterEspecialidadesTest.cs
@@ -0,0 +1,29 @@
+﻿namespace Gestao.Profissionais.Tests.Features.EspecialidadesFeature;
+
+public class ObterEspecialidadesTest
+{
+    private IRepository ObterRepository()
+    {
+        var dbContextOptions = new DbContextOptionsBuilder<ApplicationDataContext>()
+          .UseInMemoryDatabase(databaseName: "EspecialidadesDb") // Use um banco de dados em memória
+          .Options;
+        var context = new ApplicationDataContext(dbContextOptions);
+        IRepository repository = new Repository(context);
+        repository.AddAsync(new EspecialidadeEntity { Id = 1, Nome = "Pediatra", TipoDocumento = TipoDocEspecialidadeEnum.CRM }).Wait();
+        return repository;
+    }
+    [Fact]
+    public async void EspecialidadeDefault()
+    {
+        //Simula a tarefa inicial da API onde eh inserido especialidades padrao
+        var repository = ObterRepository();
+        var mockLogger = new Mock<ILogger<ObterEspecialidadesHandler>>();
+        var especialidadeProfile = new EspecialidadesMapping();
+        var configuration = new MapperConfiguration(cfg => cfg.AddProfile(especialidadeProfile));
+        IMapper mapper = new Mapper(configuration);
+        var request = new ObterEspecialidadesRequest();
+        var handler = new ObterEspecialidadesHandler(repository, mockLogger.Object, mapper);
+        var response = await handler.Handle(request, new CancellationToken());
+        Assert.True(response.Any());
+    }
+}
