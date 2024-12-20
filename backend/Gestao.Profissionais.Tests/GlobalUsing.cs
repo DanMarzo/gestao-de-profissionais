43ed9f17c187487ff14d76326d0c76eaa2e193d6
dan.gil <dan.gil@DANGIL1>
2024-12-20 12:05:09 -0300

    Inclusao de usings global

---
+++ backend/Gestao.Profissionais.Tests/GlobalUsing.cs
@@ -0,0 +1,13 @@
+﻿global using AutoMapper;
+global using Gestao.Profissionais.Application.Contracts;
+global using Microsoft.EntityFrameworkCore;
+global using Gestao.Profissionais.Application.Features.EspecialidadeFeatures.ObterEspecialidades;
+global using Gestao.Profissionais.Application.Mappings;
+global using Gestao.Profissionais.Infra.Database;
+global using Microsoft.Extensions.Logging;
+global using Moq;
+global using Gestao.Profissionais.Domain.Entities;
+global using Gestao.Profissionais.Domain.Entities.Enums;
+global using Gestao.Profissionais.Application.Features.ProfissionalFeatures.RegistrarProfissional;
+global using Gestao.Profissionais.Application.DTOs;
+global using Gestao.Profissionais.Application.Exceptions;
