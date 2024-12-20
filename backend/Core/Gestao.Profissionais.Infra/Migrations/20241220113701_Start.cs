abc253c8437a53c8fb01d2ffa17db19ed653ca09
dan.gil <dan.gil@DANGIL1>
2024-12-20 12:02:49 -0300

    Gerando Migrations

---
+++ backend/Core/Gestao.Profissionais.Infra/Migrations/20241220113701_Start.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gestao.Profissionais.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class Start : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "especialidades",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    TipoDocumento = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("especialidades_pk", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "profissionais",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    NumeroDocumento = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    EspecialidadeId = table.Column<long>(type: "INTEGER", nullable: false),
+                    CriadoEm = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("profissional_pk", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_profissionais_especialidades_EspecialidadeId",
+                        column: x => x.EspecialidadeId,
+                        principalTable: "especialidades",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_profissionais_EspecialidadeId",
+                table: "profissionais",
+                column: "EspecialidadeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "profissionais");
+
+            migrationBuilder.DropTable(
+                name: "especialidades");
+        }
+    }
+}
