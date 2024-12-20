af3110884e6f032437f969ec67b49e33c43dc233
DanMarzo <marzogildan@gmail.com>
2024-12-20 17:51:12 -0300

    Novo teste para obter profissional#

---
+++ backend/Gestao.Profissionais.Tests/Features/ProfissionalFeature/ObterProfissionalTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Gestao.Profissionais.Tests.Features.ProfissionalFeature;
+
+public class ObterProfissionalTest
+{
+    private IRepository Repository { get; set; }
+    private async Task ObterRepository()
+    {
+        var dbContextOptions = new DbContextOptionsBuilder<ApplicationDataContext>()
+          .UseInMemoryDatabase(databaseName: "ObterProfissionalTestDb") // Use um banco de dados em memória
+          .Options;
+        var context = new ApplicationDataContext(dbContextOptions);
+        this.Repository ??= new Repository(context);
+        var contagemEntidades = await this.Repository.CountAsync<EspecialidadeEntity>(x => x.Id == 1);
+        if (contagemEntidades == 0)
+            await this.Repository.AddAsync(new EspecialidadeEntity { Id = 1, Nome = "Pediatra", TipoDocumento = TipoDocEspecialidadeEnum.CRM });
+    }
+
+}
