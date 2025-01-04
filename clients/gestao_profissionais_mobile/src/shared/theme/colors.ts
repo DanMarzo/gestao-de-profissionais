75fd714ff671010c337ef7ffaf5918d063b8fce7
DanMarzo <marzogildan@gmail.com>
2025-01-03 22:11:05 -0300

    Cor padrao

---
+++ clients/gestao_profissionais_mobile/src/shared/theme/colors.ts
@@ -0,0 +1,18 @@
+type ThemeColors = {
+  success: string;
+  primary: string;
+  secondary: string;
+  warning: string;
+  tertiary: string;
+  danger: string;
+};
+const colorDefault: ThemeColors = {
+  success: '#015958',
+  primary: '#015958',
+  secondary: '#01595840',
+  warning: '#ffff00',
+  tertiary: '#6c757d',
+  danger: '#ff0000',
+};
+
+export {colorDefault};
