8e4f909cdb90b4c90ed00a92360509a0b15d4a8c
DanMarzo <marzogildan@gmail.com>
2025-01-04 00:28:45 -0300

    Exclusao temporaria de Profissional

--- clients/gestao_profissionais_mobile/src/providers/Profissional.context.tsx
+++ clients/gestao_profissionais_mobile/src/providers/Profissional.context.tsx
@@ -1,143 +1,145 @@
-import {createContext, PropsWithChildren, useEffect, useState} from 'react';
-import {ProfissionalModel} from '../models/profissional.model';
-import {ResponseListDTO} from '../infra/services/response/response-list.dto';
-import {obterProfissionaisService} from '../infra/services/obter-profissionais.service';
+// import {createContext, PropsWithChildren, useEffect, useState} from 'react';
+// import {ProfissionalModel} from '../models/profissional.model';
+// import {ResponseListDTO} from '../infra/services/response/response-list.dto';
+// import {obterProfissionaisService} from '../infra/services/obter-profissionais.service';
+// import {useRoute} from '@react-navigation/native';
 
-type AlertType = {
-  type: 'success' | 'danger';
-  message: string;
-};
+// type AlertType = {
+//   type: 'success' | 'danger';
+//   message: string;
+// };
 
-type ProfissionalContextProps = {
-  profissionais: ResponseListDTO<ProfissionalModel> | null;
-  carregando: boolean;
-  obterProfissionais: () => void;
-  nextPage: () => void;
-  previousPage: () => void;
-  obterIndicePagina: () => number;
-  handleIndiceEspecialidade: (indice: number, especialidade?: number) => void;
-  profissionalParaExcluir: ProfissionalModel | null;
-  profissionalParaAtualizar: ProfissionalModel | null;
-  handleProfissionalParaExcluir: (
-    profissional: ProfissionalModel | null,
-  ) => void;
-  handleProfissionalParaAtualizar: (
-    profissional: ProfissionalModel | null,
-  ) => void;
-  alertType: AlertType | null;
-  setAlert: (alert: AlertType | null) => void;
-};
+// type ProfissionalContextProps = {
+//   profissionais: ResponseListDTO<ProfissionalModel> | null;
+//   carregando: boolean;
+//   obterProfissionais: () => void;
+//   nextPage: () => void;
+//   previousPage: () => void;
+//   obterIndicePagina: () => number;
+//   handleIndiceEspecialidade: (indice: number, especialidade?: number) => void;
+//   profissionalParaExcluir: ProfissionalModel | null;
+//   profissionalParaAtualizar: ProfissionalModel | null;
+//   handleProfissionalParaExcluir: (
+//     profissional: ProfissionalModel | null,
+//   ) => void;
+//   handleProfissionalParaAtualizar: (
+//     profissional: ProfissionalModel | null,
+//   ) => void;
+//   alertType: AlertType | null;
+//   setAlert: (alert: AlertType | null) => void;
+// };
 
-const ProfissionalContext = createContext<ProfissionalContextProps>(
-  {} as ProfissionalContextProps,
-);
+// const ProfissionalContext = createContext<ProfissionalContextProps>(
+//   {} as ProfissionalContextProps,
+// );
 
-const ProfissionalProvider = ({children}: PropsWithChildren) => {
-  const [alertType, setAlertType] = useState<null | AlertType>(null);
-  const [profissionais, setProfissionais] =
-    useState<ResponseListDTO<ProfissionalModel> | null>(null);
-  const [searchParams, setSearchParams] = useSearchParams();
+// const ProfissionalProvider = ({children}: PropsWithChildren) => {
+//   const [alertType, setAlertType] = useState<null | AlertType>(null);
+//   const [profissionais, setProfissionais] =
+//     useState<ResponseListDTO<ProfissionalModel> | null>(null);
+//   const {params} = useRoute();
 
-  const [carregando, setCarregando] = useState(false);
+//   const [carregando, setCarregando] = useState(false);
 
-  const [profissionalParaAtualizar, setProfissionalParaAtualizar] =
-    useState<ProfissionalModel | null>(null);
-  const [profissionalParaExcluir, setProfissionalParaExcluir] =
-    useState<ProfissionalModel | null>(null);
-  const obterProfissionais = () => {
-    const indice = obterIndicePagina();
-    const especialidadeId = obterEspecialidadeId();
-    setCarregando(true);
-    obterProfissionaisService(indice, especialidadeId)
-      .then(res => {
-        if (!res.error) {
-          setProfissionais(res);
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        //toast("Não foi possível carregar profissionais.", { type: "error" });
-        setProfissionais(null);
-      })
-      .finally(() => setCarregando(false));
-  };
+//   const [profissionalParaAtualizar, setProfissionalParaAtualizar] =
+//     useState<ProfissionalModel | null>(null);
+//   const [profissionalParaExcluir, setProfissionalParaExcluir] =
+//     useState<ProfissionalModel | null>(null);
+//   const obterProfissionais = () => {
+//     const indice = obterIndicePagina();
+//     const especialidadeId = obterEspecialidadeId();
+//     setCarregando(true);
+//     obterProfissionaisService(indice, especialidadeId)
+//       .then(res => {
+//         if (!res.error) {
+//           setProfissionais(res);
+//         }
+//       })
+//       .catch(err => {
+//         console.log(err);
+//         //toast("Não foi possível carregar profissionais.", { type: "error" });
+//         setProfissionais(null);
+//       })
+//       .finally(() => setCarregando(false));
+//   };
 
-  const setAlert = (alert: AlertType | null) => {
-    setAlertType(alert);
-  };
+//   const setAlert = (alert: AlertType | null) => {
+//     setAlertType(alert);
+//   };
 
-  const obterIndicePagina = () => {
-    const indice = searchParams.get('indice');
-    if (!indice) return 1;
-    return Number.parseInt(indice);
-  };
+//   const obterIndicePagina = () => {
 
-  const obterEspecialidadeId = () => {
-    const especialidade = searchParams.get('especialidade');
-    if (!especialidade) return undefined;
-    return Number.parseInt(especialidade);
-  };
-  const nextPage = () => {
-    if (
-      profissionais &&
-      !profissionais.error &&
-      obterIndicePagina() < profissionais.nroPaginas
-    ) {
-      setSearchParams({indice: (obterIndicePagina() + 1).toString()});
-    }
-  };
-  const previousPage = () => {
-    if (profissionais && !profissionais.error && obterIndicePagina() > 1) {
-      setSearchParams({indice: (obterIndicePagina() - 1).toString()});
-    }
-  };
-  const handleIndiceEspecialidade = (
-    indice: number,
-    especialidade?: number,
-  ) => {
-    setSearchParams({
-      indice: indice.toString(),
-      especialidade: especialidade ? especialidade.toString() : '',
-    });
-  };
+//     if (!indice) return 1;
+//     return Number.parseInt(indice);
+//   };
 
-  const handleProfissionalParaExcluir = (
-    profissional: ProfissionalModel | null,
-  ) => {
-    setProfissionalParaExcluir(profissional);
-  };
+//   const obterEspecialidadeId = () => {
+//     const especialidade = searchParams.get('especialidade');
+//     if (!especialidade) return undefined;
+//     return Number.parseInt(especialidade);
+//   };
+//   const nextPage = () => {
+//     if (
+//       profissionais &&
+//       !profissionais.error &&
+//       obterIndicePagina() < profissionais.nroPaginas
+//     ) {
+//       setSearchParams({indice: (obterIndicePagina() + 1).toString()});
+//     }
+//   };
+//   const previousPage = () => {
+//     if (profissionais && !profissionais.error && obterIndicePagina() > 1) {
+//       setSearchParams({indice: (obterIndicePagina() - 1).toString()});
+//     }
+//   };
+//   const handleIndiceEspecialidade = (
+//     indice: number,
+//     especialidade?: number,
+//   ) => {
+//     setSearchParams({
+//       indice: indice.toString(),
+//       especialidade: especialidade ? especialidade.toString() : '',
+//     });
+//   };
 
-  const handleProfissionalParaAtualizar = (
-    profissional: ProfissionalModel | null,
-  ) => {
-    setProfissionalParaAtualizar(profissional);
-  };
+//   const handleProfissionalParaExcluir = (
+//     profissional: ProfissionalModel | null,
+//   ) => {
+//     setProfissionalParaExcluir(profissional);
+//   };
 
-  useEffect(() => {
-    obterProfissionais();
-    return () => {};
-  }, [searchParams]);
-  return (
-    <ProfissionalContext.Provider
-      value={{
-        setAlert,
-        handleProfissionalParaAtualizar,
-        handleProfissionalParaExcluir,
-        handleIndiceEspecialidade,
-        obterIndicePagina,
-        profissionais,
-        carregando,
-        obterProfissionais,
-        nextPage,
-        previousPage,
-        profissionalParaAtualizar,
-        profissionalParaExcluir,
-        alertType,
-      }}>
-      {children}
-    </ProfissionalContext.Provider>
-  );
-};
+//   const handleProfissionalParaAtualizar = (
+//     profissional: ProfissionalModel | null,
+//   ) => {
+//     setProfissionalParaAtualizar(profissional);
+//   };
 
-export type {ProfissionalContextProps};
-export {ProfissionalProvider, ProfissionalContext};
+//   useEffect(() => {
+//     obterProfissionais();
+//     return () => {};
+//   }, [params]);
+
+//   return (
+//     <ProfissionalContext.Provider
+//       value={{
+//         setAlert,
+//         handleProfissionalParaAtualizar,
+//         handleProfissionalParaExcluir,
+//         handleIndiceEspecialidade,
+//         obterIndicePagina,
+//         profissionais,
+//         carregando,
+//         obterProfissionais,
+//         nextPage,
+//         previousPage,
+//         profissionalParaAtualizar,
+//         profissionalParaExcluir,
+//         alertType,
+//       }}>
+//       {children}
+//     </ProfissionalContext.Provider>
+//   );
+// };
+
+// export type {ProfissionalContextProps};
+// export {ProfissionalProvider, ProfissionalContext};
