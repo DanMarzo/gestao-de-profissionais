161ec2b3f8c0de203251d56492b49817c7ecccc4
DanMarzo <marzogildan@gmail.com>
2025-01-03 21:30:28 -0300

    Rotas iniciais

--- clients/gestao_profissionais_mobile/App.tsx
+++ clients/gestao_profissionais_mobile/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import {useColorScheme} from 'react-native';
-import {NavigationContainer, NavigationProp} from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import HomePage from './src/views/home/HomePage';
 import RegistrarProfissionaisPage from './src/views/home/RegistrarProfissionaisPage';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 export type RootStackParamList = {
   HomePage: undefined;
   RegistrarProfissionaisPage: {value: string};
 };
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
