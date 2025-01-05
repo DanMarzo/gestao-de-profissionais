3dab86baea3cb1767c2332d4e8c45b3f79c7e71a
DanMarzo <marzogildan@gmail.com>
2025-01-05 15:22:01 -0300

    Realizando formatacao de arquivo e dto para registrar profissional

--- clients/gestao_profissionais_mobile/src/models/profissional.model.ts
+++ clients/gestao_profissionais_mobile/src/models/profissional.model.ts
@@ -1,5 +1,5 @@
-import * as yup from "yup";
-import { EspecialidadeModel } from "./especialidade.model";
+import * as yup from 'yup';
+import {EspecialidadeModel} from './especialidade.model';
 
 class ProfissionalModel {
   id: number;
@@ -9,16 +9,23 @@ class ProfissionalModel {
   criadoEm: string;
   especialidade: EspecialidadeModel;
 }
+
+class RegistrarProfissionalDTO {
+  especialidadeId: number;
+  nome: string;
+  numeroDocumento: string;
+}
+
 const formProfissionalSchema = yup.object().shape({
   nome: yup
     .string()
-    .required("Nome do profissional é obrigatorio")
-    .min(3, "Minimo 3 caracteres"),
+    .required('Nome do profissional é obrigatorio')
+    .min(3, 'Minimo 3 caracteres'),
   numeroDocumento: yup
     .string()
-    .required("Número do documento é obrigatorio.")
-    .min(3, "Minimo 3 caracteres"),
-  especialidadeId: yup.number().required(),
+    .required('Número do documento é obrigatorio.')
+    .min(3, 'Minimo 3 caracteres'),
+  especialidadeId: yup.number().required('Especialidade é obrigatório.'),
 });
 
-export { ProfissionalModel, formProfissionalSchema };
+export {ProfissionalModel, formProfissionalSchema, RegistrarProfissionalDTO};
