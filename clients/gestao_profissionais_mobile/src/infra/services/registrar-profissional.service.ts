05b383b95fc202473c639756f500d6ddb32c6358
DanMarzo <marzogildan@gmail.com>
2025-01-05 15:21:18 -0300

    Incluindo tipagem para registrar profissional

--- clients/gestao_profissionais_mobile/src/infra/services/registrar-profissional.service.ts
+++ clients/gestao_profissionais_mobile/src/infra/services/registrar-profissional.service.ts
@@ -1,9 +1,10 @@
+import { RegistrarProfissionalDTO } from "../../models/profissional.model";
 import { getHttp } from "../http";
 import { ResponseToCreate } from "./response/response-to-create.dto";
 import { ResponseAPIDTO } from "./response/response.api.dto";
 
 const registrarProfissionalService = async (
-  values: any
+  values: RegistrarProfissionalDTO
 ): Promise<ResponseAPIDTO<ResponseToCreate<string>>> => {
   const response = await (await getHttp()).post("/api/Profissional", values);
   return new ResponseAPIDTO<ResponseToCreate<string>>(response);
