cb27c2bccfc2e6e14d4ed4374ea37dddbe0b6a74
DanMarzo <marzogildan@gmail.com>
2025-01-05 15:23:34 -0300

    Organizando para registrar profissionais

--- clients/gestao_profissionais_mobile/src/views/profissionais/registrar/registrar-profissionais.view-model.ts
+++ clients/gestao_profissionais_mobile/src/views/profissionais/registrar/registrar-profissionais.view-model.ts
@@ -1,42 +1,81 @@
 import {useForm} from 'react-hook-form';
 import {yupResolver} from '@hookform/resolvers/yup';
-import {formProfissionalSchema} from '../../../models/profissional.model';
-import {useContext, useState} from 'react';
+import {
+  formProfissionalSchema,
+  RegistrarProfissionalDTO,
+} from '../../../models/profissional.model';
+import {useContext, useEffect, useState} from 'react';
 import {EspecialidadeContext} from '../../../providers/Especialidade.context';
 import {IndexPath} from '@ui-kitten/components';
+import {EspecialidadeModel} from '../../../models/especialidade.model';
+import {registrarProfissionalService} from '../../../infra/services/registrar-profissional.service';
+import {Toast} from '../../../shared/theme/toasts';
+import {ToastAndroid} from 'react-native';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {RootStackParamList} from '../../../../App';
 
-const useRegistrarProssionalViewModel = () => {
+const useRegistrarProfissionalViewModel = () => {
   const {
-    register,
+    setValue: setValueForm,
     handleSubmit,
-    reset,
     control: controlForm,
     formState: {errors: errorsForm},
   } = useForm({resolver: yupResolver(formProfissionalSchema)});
-  const [tipoDocField, setTipoDocField] = useState<string | null>('');
+  const {goBack} = useNavigation<NavigationProp<RootStackParamList>>();
   const [carregando, setCarregando] = useState(false);
-  const [especialidadeSelect, setEspecialidadeSelect] = useState<number | null>(
-    null,
-  );
-  const [selectedIndex, setSelectedIndex] = useState<IndexPath>(
-    new IndexPath(0),
-  );
+  const [selectedIndex, setSelectedIndex] = useState<IndexPath>(new IndexPath(0));
+  const [especialidade, setEspecialidade] = useState<EspecialidadeModel | undefined>(undefined);
   const especialidadeContext = useContext(EspecialidadeContext);
 
-  const registrarProfissional = (values: any) => {
-    console.log(values);
+  const registrarProfissional = async (
+    novoProfissional: RegistrarProfissionalDTO,
+  ) => {
+    try {
+      setCarregando(true);
+      const result = await registrarProfissionalService(novoProfissional);
+      if (!result.error) {
+        Toast(
+          `Profissional ${novoProfissional.nome} - Doc. ${novoProfissional.numeroDocumento} Registrado!`,
+          ToastAndroid.TOP,
+        );
+        goBack();
+      } else {
+        Toast('Não foi possível registrar profissional', ToastAndroid.TOP);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setCarregando(false);
+    }
   };
+
+  const handleEspecialidade = (index: IndexPath) => {
+    let espec = especialidadeContext.especialidades[index.row];
+    setEspecialidade(espec);
+    setValueForm('especialidadeId', espec.id);
+  };
+
+  useEffect(() => {
+    if (especialidade) {
+      let index = especialidadeContext.especialidades.indexOf(especialidade);
+      setSelectedIndex(new IndexPath(index));
+    }
+    return () => {};
+  }, []);
 
   return {
     controlForm,
     errorsForm,
     handleSubmit,
-    register,
     registrarProfissional,
     especialidades: especialidadeContext.especialidades,
     carregandoEspecialidade: especialidadeContext.carregando,
-    selectedIndex, setSelectedIndex
+    selectedIndex,
+    setSelectedIndex,
+    handleEspecialidade,
+    especialidade,
+    carregando,
   };
 };
 
-export {useRegistrarProssionalViewModel};
+export {useRegistrarProfissionalViewModel};
