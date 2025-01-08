204180e4c4e0e6baeeed2cbb25dfe81f3e711c50
DanMarzo <marzogildan@gmail.com>
2025-01-07 22:18:30 -0300

    Inclusao de metodo para recuperar parametros

--- clients/gestao_profissionais_mobile/src/views/profissionais/atualizar/AtualizarProfissionalPage.tsx
+++ clients/gestao_profissionais_mobile/src/views/profissionais/atualizar/AtualizarProfissionalPage.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {ProfissionalModel} from '../../../models/profissional.model';
+import {RouteProp, useRoute} from '@react-navigation/native';
+
+type PropsAtualizarProfissional = {profissional: ProfissionalModel};
 
 const AtualizarProfissionalPage = () => {
+  const {params} = useRoute<RouteProp<PropsAtualizarProfissional>>();
   return (
     <View>
-      <Text>AtualizarProfissionalPage</Text>
+      <Text>{JSON.stringify(params)}</Text>
     </View>
   );
 };
 
 export {AtualizarProfissionalPage};
+export type {PropsAtualizarProfissional};
