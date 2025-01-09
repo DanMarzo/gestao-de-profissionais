942d34c91369479f80ae7c1debeea57c995d0340
DanMarzo <marzogildan@gmail.com>
2025-01-09 11:05:14 -0300

    Comentarios na config de redux

--- clients/gestao_profissionais_mobile/src/infra/config-redux.ts
+++ clients/gestao_profissionais_mobile/src/infra/config-redux.ts
@@ -2,6 +2,9 @@ import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import {especialidadeReducer} from '../shared/state/especialidade/especialidade.state';
 import { State } from '../types';
 
+//Sim eu sei, usar Context API teria sido mt melhor, mais facil, iria diminuir muito trabalho
+//Mas fazia mt tempo que nao usava redux e para praticar optei por usar ele
+
 const reducers = combineReducers({
   especialidade: especialidadeReducer,
 });
