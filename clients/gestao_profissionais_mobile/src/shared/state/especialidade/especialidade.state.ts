4d325c66d1eba74cb417075c5f5db11058c6838f
DanMarzo <marzogildan@gmail.com>
2025-01-09 15:26:19 -0300

    Inclusao de novo tipo e de Thunk

--- clients/gestao_profissionais_mobile/src/shared/state/especialidade/especialidade.state.ts
+++ clients/gestao_profissionais_mobile/src/shared/state/especialidade/especialidade.state.ts
@@ -1,21 +1,43 @@
+import {obterEspecialidadesService} from '../../../infra/services/obter-especialidades.service';
 import {EspecialidadeModel} from '../../../models/especialidade.model';
-import {PayloadAction, createSlice} from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 
 type EspecialidadeState = {
   especialidades: Array<EspecialidadeModel>;
+  carregando: boolean;
 };
 const initialState: EspecialidadeState = {
-  especialidades: []
+  especialidades: [],
+  carregando: false,
 };
+const getEspecialidadesAction = createAsyncThunk(
+  'getEspecialidadesAction',
+  async () => {
+    const response = await obterEspecialidadesService();
+    return response.data;
+  },
+);
+
 const especialidade = createSlice({
   name: 'especialidade',
   initialState,
-  reducers: {
-    setEspecialidades: (state, action: PayloadAction<Array<EspecialidadeModel>>) => {
-        state.especialidades = action.payload;
-    },
+  reducers: {},
+  extraReducers: builder => {
+    builder.addCase(getEspecialidadesAction.fulfilled, (state, action) => {
+      const lista = action?.payload;
+      state.especialidades = lista ?? [];
+      state.carregando = false;
+    }),
+      builder.addCase(getEspecialidadesAction.pending, (state, action) => {
+        state.carregando = true;
+      }),
+      builder.addDefaultCase((state, action) => {
+        state.carregando = false;
+      });
   },
 });
 
-export const { actions: especialidadeActions, reducer: especialidadeReducer } = especialidade;
+export const {actions: especialidadeActions, reducer: especialidadeReducer} =
+  especialidade;
+export {getEspecialidadesAction};
 export type {EspecialidadeState};
