5d39061f9ba6bc5a49031ef602f1386dd5829219
DanMarzo <marzogildan@gmail.com>
2025-01-09 09:03:58 -0300

    Inclusao de Provider

--- clients/gestao_profissionais_mobile/App.tsx
+++ clients/gestao_profissionais_mobile/App.tsx
@@ -5,6 +5,9 @@ import themeLight from './theme.json';
 import {MD3LightTheme, PaperProvider} from 'react-native-paper';
 import {ThemeProp} from 'react-native-paper/lib/typescript/types';
 import {Routes} from './src/views/route';
+import {Provider as ReduxProvider} from 'react-redux';
+import {store} from './src/infra/config-redux';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 const theme: ThemeProp = {
   ...MD3LightTheme,
@@ -15,15 +18,27 @@ const theme: ThemeProp = {
   },
 };
 
+const configQueryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 const App = (): React.JSX.Element => {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <PaperProvider theme={theme}>
-      <EspecialidadeProvider>
-        <Routes />
-      </EspecialidadeProvider>
-    </PaperProvider>
+    <QueryClientProvider client={configQueryClient}>
+      <ReduxProvider store={store}>
+        <PaperProvider theme={theme}>
+          <EspecialidadeProvider>
+            <Routes />
+          </EspecialidadeProvider>
+        </PaperProvider>
+      </ReduxProvider>
+    </QueryClientProvider>
   );
 };
 
