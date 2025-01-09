e86b923c56d89cd9bcb9e8c8961b7eb9c0b2d565
DanMarzo <marzogildan@gmail.com>
2025-01-09 15:25:42 -0300

    Configurando dispatch para operar com Thunk

--- clients/gestao_profissionais_mobile/src/infra/config-redux.ts
+++ clients/gestao_profissionais_mobile/src/infra/config-redux.ts
@@ -1,6 +1,7 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import {especialidadeReducer} from '../shared/state/especialidade/especialidade.state';
-import { State } from '../types';
+import {State} from '../types';
+import {useDispatch} from 'react-redux';
 
 //Sim eu sei, usar Context API teria sido mt melhor, mais facil, iria diminuir muito trabalho
 //Mas fazia mt tempo que nao usava redux e para praticar optei por usar ele
@@ -14,4 +15,6 @@ const store = configureStore<State>({
   reducer: reducers,
 });
 
-export {store, reducers};
+const useAppDispatch = () => useDispatch<typeof store.dispatch>();
+
+export {store, reducers, useAppDispatch};
