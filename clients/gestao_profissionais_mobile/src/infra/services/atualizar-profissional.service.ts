3d1e973a81b06e0fa06692f18a29ad6b363b8bff
DanMarzo <marzogildan@gmail.com>
2025-01-09 11:06:43 -0300

    Definindo tipagem

--- clients/gestao_profissionais_mobile/src/infra/services/atualizar-profissional.service.ts
+++ clients/gestao_profissionais_mobile/src/infra/services/atualizar-profissional.service.ts
@@ -1,10 +1,10 @@
-import { ProfissionalModel } from "../../models/profissional.model";
+import { AtualizarProfissionalDTO, ProfissionalModel } from "../../models/profissional.model";
 import { getHttp } from "../http";
 import { ResponseAPIDTO } from "./response/response.api.dto";
 
 const atualizarProfissionalService = async (
   idProfissional: number,
-  values: any
+  values: AtualizarProfissionalDTO
 ): Promise<ResponseAPIDTO<ProfissionalModel>> => {
   const response = await (await getHttp()).put(
     `/api/Profissional/${idProfissional}`,
