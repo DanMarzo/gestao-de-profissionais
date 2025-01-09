f36e6d6bd5ffa944cd53c6a4fd4b299a15c28665
DanMarzo <marzogildan@gmail.com>
2025-01-09 15:15:35 -0300

    Inclusao de construtor

--- backend/Core/Gestao.Profissionais.Domain/Entities/EspecialidadeEntity.cs
+++ backend/Core/Gestao.Profissionais.Domain/Entities/EspecialidadeEntity.cs
@@ -2,6 +2,13 @@
 
 public sealed class EspecialidadeEntity
 {
+    public EspecialidadeEntity() { }
+    public EspecialidadeEntity(long id, string nome, TipoDocEspecialidadeEnum tipoDocumento)
+    {
+        this.Id = id;
+        this.Nome = nome;
+        this.TipoDocumento = tipoDocumento;
+    }
     public long Id { get; set; }
     public string Nome { get; set; }
     public TipoDocEspecialidadeEnum TipoDocumento { get; set; }
